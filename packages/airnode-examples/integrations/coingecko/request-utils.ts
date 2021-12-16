1e086de75d8e811333b0dcd261f9e53c596e124e
jasonzh <jasonzhagui@gmail.com>
2021-12-16 00:06:19 -0500

    Update request-utils.ts

--- packages/airnode-examples/integrations/coingecko/request-utils.ts
+++ packages/airnode-examples/integrations/coingecko/request-utils.ts
@@ -1,8 +1,9 @@
+
 import { encode } from '@api3/airnode-abi';
 import { cliPrint, getDeployedContract, readIntegrationInfo } from '../../src';
 
 const coinLabel = 'Ethereum';
-const coinId = coinLabel.toLowerCase();
+const coinId = 'ethusdt';
 
 export const getEncodedParameters = () => {
   return encode([{ name: 'coinId', type: 'bytes32', value: coinId }]);
@@ -12,6 +13,5 @@ export const printResponse = async (requestId: string) => {
   const integrationInfo = readIntegrationInfo();
   const requester = await getDeployedContract(`contracts/${integrationInfo.integration}/Requester.sol`);
 
-  // Divided by 1e6, because the response value is multiplied with 1e6 by Airnode
   cliPrint.info(`${coinLabel} price is ${(await requester.fulfilledData(requestId)) / 1e6} USD`);
 };
